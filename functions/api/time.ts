d395d70eee24ba075dbec6d6f3f2875196620c7e
Alex MacCaw <alex@alexmaccaw.com>
2023-04-30 21:19:51 -0400

    test

--- functions/api/time.ts
+++ functions/api/time.ts
@@ -1,12 +1,16 @@
-export const onRequestGet: PagesFunction = async () => {
-	return new Response(
-		JSON.stringify({
-			time: new Date().toISOString(),
-		}),
-		{
-			headers: {
-				'content-type': 'application/json',
-			},
-		}
-	);
-};
+interface Env {
+  TEST: string
+}
+
+export const onRequestGet: PagesFunction<Env> = async () => {
+  return new Response(
+    JSON.stringify({
+      time: new Date().toISOString(),
+    }),
+    {
+      headers: {
+        'content-type': 'application/json',
+      },
+    }
+  )
+}
